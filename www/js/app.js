f9b7eb1f6f259e32b9fa47f74a0e23b722a298e1
ksieee <ksieee@gmail.com>
2015-06-25 22:57:13 +0800

    Add shop-detail.html
    Signed-off-by: ksieee <ksieee@gmail.com>

--- www/js/app.js
+++ www/js/app.js
@@ -10,10 +10,11 @@ angular.module('akala', ['ionic', 'akala.services', 'akala.directives', 'akala.f
             StatusBar.styleLightContent();
         }
     });
-}).run(function ($rootScope, $http, $window, UserSrv, Router2Console, EnableBackBtn) {
+}).run(function ($rootScope, $http, $window, UserSrv, Router2Console, EnableBackBtn, EnableLightHeader) {
     UserSrv.logonWithLocalUser();
     Router2Console.active = $window.akala.enableDebug;
-    EnableBackBtn.enableBackStateNames = ['address'];
+    EnableBackBtn.enableBackStateNames = ['address', 'shop-detail'];
+    EnableLightHeader.enableLightHeaderStateNames = ['shop-detail'];
 }).constant('$ionicLoadingConfig', {
     template: '<ion-spinner class="spinner-balanced"></ion-spinner>'
 }).config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
@@ -103,15 +104,10 @@ angular.module('akala', ['ionic', 'akala.services', 'akala.directives', 'akala.f
             controller: 'AddressMapCtrl'
         })
 
-        .state('tab.shop-detail', {
+        .state('shop-detail', {
             url: '/shop-detail',
-            views: {
-                'tab-shop': {
-                    templateUrl: 'templates/shop/shop-detail.html',
-                    controller: 'ShopDetailCtrl'
-                }
-            }
-
+            templateUrl: 'templates/shop/shop-detail.html',
+            controller: 'ShopDetailCtrl'
         });
 
     // if none of the above states are matched, use this as the fallback
